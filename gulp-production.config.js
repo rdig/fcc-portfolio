f7d51a1226651c435ec23f46c3f3851a6dec4cf4
Raul Glogovetan <raul@glogovetan.com>
2016-02-02 07:59:33 +0200

    Optimized the gulp build pipe, created gulp production build pipe

--- gulp-production.config.js
+++ gulp-production.config.js
@@ -1,14 +1,10 @@
 var gulp = require('gulp');
 var del = require('del');
-var connect = require('gulp-connect');
 var babel = require('gulp-babel');
 var sass = require('gulp-sass');
-var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
 
 var config = {
-	// Port number under 1024 on linux requires root privileges
-	port: 8080,
-	url: 'http://localhost',
 	paths: {
 		src: {
 			js: {
@@ -39,69 +35,42 @@ gulp.task('Cleanup', function() {
 	]);
 });
 
-// Server task
-gulp.task('Local web server', ['Cleanup'], function() {
-	connect.server({
-		root: ['./'],
-		port: config.port,
-		base: config.url,
-		livereload: true
-	});
-});
-
 // Process javascript libraries task
-gulp.task('Process JS libraries', function() {
+gulp.task('Process JS libraries', ['Cleanup'], function() {
 	gulp.src(config.paths.src.js.libs)
-		.pipe(gulp.dest(config.paths.dest.js.libs))
-		.pipe(connect.reload());
+		.pipe(gulp.dest(config.paths.dest.js.libs));
 });
 
 // Transpile the main scripts via babel / ES2015
 // Info: https://www.npmjs.com/package/gulp-babel
-gulp.task('Transpile JS', function() {
+// Info: https://github.com/terinjokes/gulp-uglify
+gulp.task('Transpile JS', ['Cleanup'], function() {
 	gulp.src(config.paths.src.js.scripts)
 		.pipe(babel({ presets: ['es2015'] }))
 		.on('error', console.error.bind(console))
-		.pipe(gulp.dest(config.paths.dest.js.scripts))
-		.pipe(connect.reload());
-});
-
-// Lint the JS files. Rules -> eslint-rules.json
-gulp.task('Lint JS', function() {
-	gulp.src(config.paths.src.js.scripts)
-		.pipe(eslint({ config: 'eslint-rules.json'}))
-		.pipe(eslint.format())
-		.pipe(connect.reload());
+		.pipe(uglify({ mangle: true }))
+		.pipe(gulp.dest(config.paths.dest.js.scripts));
 });
 
 // Process all the images, basically just move them, as for this project we are using
 // already compressed svgz images, there is no need to do anything to them
-gulp.task('Process Media', function() {
+gulp.task('Process Media', ['Cleanup'], function() {
 	// filter images / media files so we don't process anything else that might end
 	// up in the media folder, although it shoudn't
 	var filteredMediaFiles = ['ico', 'gif', 'jpg', 'jpeg', 'png', 'svg', 'svgz'].map(function(ext) {
 		return config.paths.src.media + '.' + ext;
 	});
-	gulp.src(filteredMediaFiles).pipe(gulp.dest(config.paths.dest.media)).pipe(connect.reload());
+	gulp.src(filteredMediaFiles).pipe(gulp.dest(config.paths.dest.media));
 });
 
 // Preprocess the SCSS stylesheets
 // Info: https://www.npmjs.com/package/gulp-sass
-gulp.task('Preprocess SCSS', function() {
+gulp.task('Preprocess SCSS', ['Cleanup'], function() {
 	// Files like _*.scss are auto filtered by the node-sass pipe.
 	gulp.src(config.paths.src.scss)
-		.pipe(sass())
+		.pipe(sass({ outputStyle: 'compressed' }))
 		.on('error', sass.logError)
-		.pipe(gulp.dest(config.paths.dest.scss))
-		.pipe(connect.reload());
-});
-
-// Watch for changes
-gulp.task('File watcher', function() {
-	gulp.watch(config.paths.src.js.libs, ['Process JS libraries']);
-	gulp.watch(config.paths.src.js.scripts, ['Transpile JS', 'Lint JS']);
-	gulp.watch(config.paths.src.media, ['Process Media']);
-	gulp.watch(config.paths.src.scss, ['Preprocess SCSS']);
+		.pipe(gulp.dest(config.paths.dest.scss));
 });
 
 // Default task
@@ -110,10 +79,12 @@ gulp.task(
 	[
 		'Process JS libraries',
 		'Transpile JS',
-		'Lint JS',
 		'Process Media',
 		'Preprocess SCSS',
-		'Local web server',
-		'File watcher'
-	]
+	],
+	function() {
+		console.log('\n');
+		console.log('Production build completed successfully');
+		console.log('\n');
+	}
 );
